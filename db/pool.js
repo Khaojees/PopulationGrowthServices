2c9acce2f7ab17a2a39844c1306cfea8fc858de8
SawangS <poolohit@outlook.com>
2023-11-01 16:03:54 +0700

    vercel

--- db/pool.js
+++ db/pool.js
@@ -1,10 +1,19 @@
 const Pool = require('pg').Pool
+// const pool = new Pool({
+//       user:"postgres",
+//       password:"admin",
+//       host:"localhost",
+//       port:9999,
+//       database:"population"
+// })
+
 const pool = new Pool({
-      user:"postgres",
-      password:"admin",
-      host:"localhost",
-      port:9999,
-      database:"population"
-})
+      connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+    })
+
+    pool.connect((err)=>{
+      if(err) throw err
+      console.log("Conected")
+    })
 
 module.exports = pool
